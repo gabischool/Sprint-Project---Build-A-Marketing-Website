5177bd3f0cf96c74f515f67186032a509a6383c7
Duraan Ali <16447314+duraanali@users.noreply.github.com>
2021-02-05 08:40:16 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 // Add your Javascript work here
+
+// Task 1: Create a basic Calculator
+
+/* 
+
+1. Start creating three prompts, the first two for the two numbers and the second one for the operation
+2. Also declare another variable for the result.
+3. Using conditional statement, check if the operation is addition, subtraction, division or multipilcation
+4. Add the output to the result variable you created.
+5. Console.log the result.
